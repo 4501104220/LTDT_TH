c94a52ac6c9b910ceb7002637dab379828d1dcbb
4501104220 <72953715+4501104220@users.noreply.github.com>
2020-12-04 16:10:10 +0700

    Update Main.cpp

--- LTDT_TH04/LTDT_TH04/Main.cpp
+++ LTDT_TH04/LTDT_TH04/Main.cpp
@@ -14,7 +14,7 @@ struct CANH
 	int trongso;
 };
 
-CANH T[100];//Mang luu các canh trong thuat toán Prim
+CANH T[100];//Mang luu cac canh trong thuat toan Prim
 
 void nhapMaTranKe(string duongdan, GRAPH& dt) {
 	ifstream inp;
@@ -29,32 +29,33 @@ void nhapMaTranKe(string duongdan, GRAPH& dt) {
 		inp.close();
 	}
 	else {
-		cout << "Khong mo duoc file";
+		cout << "Khong mo duoc file!";
 		exit(0);
 	}
 }
 void Prim(char v, GRAPH g, bool type)
 {
-	//B1: Gán so canh cua cây khung ban dau là 0
+	//B1: Gan so canh cua cay khung ban dau là 0
 	int nT = 0;
-	//B2: Khoi tao nhãn các dinh là chua duyet (0)
+	//B2: Khoi tao nhan cac dinh la chua duyet (0)
 	for (int i = 0; i < g.iSoDinh; i++)
 		g.visited[i] = 0;
 
-	//B3: Ðánh dau dinh 0 là dã duyet
+	//B3: Danh dau dinh 0 la da duyet
 	g.visited[v - 'A'] = 1;
 
-	while (nT < g.iSoDinh - 1)//neu bang n -1 canh thì dung (*)	
+	while (nT < g.iSoDinh - 1)//neu bang n -1 canh thi dung	vi nT xet tu 0 va neu nT == n(so dinh) - 1 thi co nghia la thuat toan da duyet qua tat ca cac dinh, vay nen dung thuat toan
 	{
-		CANH canhnhonhat;//dùng de luu tru canh nho nhat tu mot dinh da xét toi dinh chua xét
-		int tmp = -1;	//(*)
+		CANH canhnhonhat;//dung de luu tru canh nho nhat tu mot dinh da xét toi dinh chua xet
+		int tmp = -1;	//tmp = -1 boi vi trong so ban dau theo bai la luon duong, nen tmp = -1 se khong lam anh huong den thuat toan
 		for (int i = 0; i < g.iSoDinh; i++)
-			if (g.visited[i] == 0) //neu là dinh chua duyet. Ðieu kien dúng khi j thuoc tap Y hay V\Y
+			if (g.visited[i] == 0) //neu la dinh chua duyet. Dieu kien dung khi j thuoc tap Y hay V\Y
 			{
 				for (int j = 0; j < g.iSoDinh; j++)
 					if (g.visited[j] == 1 && (g.iMaTran[i][j] != 0))
 					{
 						if ((type == true && (tmp == -1 || g.iMaTran[i][j] < tmp)) || (type == false && (tmp == -1 || g.iMaTran[i][j] > tmp)))
+							//type == true duyet cau khung no nhat, type == false duyet cay khung lon nhat
 						{
 							tmp = g.iMaTran[i][j];
 							canhnhonhat.u = i;
@@ -64,15 +65,15 @@ void Prim(char v, GRAPH g, bool type)
 					}
 			}
 
-		//Thêm canh nho nhat vào tap T
+		//Them canh nho nhat vao tap T
 		T[nT] = canhnhonhat;
-		nT++;//tang so canh lên 1
+		nT++;//Tang so canh len 1
 
-		g.visited[canhnhonhat.u] = 1;//(*)
+		g.visited[canhnhonhat.u] = 1;//De danh dau nhan biet la dinh do da duyet rui va da luu tru vao tap Y
 
 	}
 	char c = 'A';
-	//T?ng tr?ng s? c?a cây khung
+	//Tang trong so cua cay khung
 	int sum = 0;
 	if (type) cout << "\nCay khung nho nhat cua do thi la: " << endl;
 	else cout << "\nCay khung lon nhat cua do thi la: " << endl;
@@ -91,7 +92,7 @@ int main() {
 	nhapMaTranKe(duongdan, g);
 
 	while (true) {
-		cout << "nhap dinh bat dau: (vd A B C D)\n";
+		cout << "Nhap dinh bat dau: (vd: A)\n";
 		cin >> c;
 		if (c >= 'A' && c <= char(65 + g.iSoDinh - 1)) {
 			Prim(c, g, true);
@@ -103,15 +104,3 @@ int main() {
 		}
 	}
 }
-//Cau 3 (*) Giai thich
-//Dong 44: tai vi nT xet tu 0 va neu nT == n(so dinh) - 1 thi co nghia la thuat toan da duyet qua tat ca cac dinh, nen dung thuat toan.
-//dong 47: tmp = -1 boi vi trong so ban dau theo bai la luon duong nen tmp = -1 se khong lam anh huong den thuat toan
-//Dong 68: De danh dau nhan biet la dinh do da duyet rui va da luu tru vao tap Y
-/* Cau 2:
-buoc1 : 38 - 44
-buoc2 : 46 - 64
-buoc3 : 67 - 70
-// Cau 4 : trong ham Prim(GRAPH g, bool type) type == true duyet cau khung no nhat, type == false duyet cay khung lon nhat
-//Cau 6:
-Thuat toan tren van chay duoc voi trong so am, tru khi thay vi bien tmp = -1 thì tmp nen bang so vo cung lon hoac vo cung be
-*/
